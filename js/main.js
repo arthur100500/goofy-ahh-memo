44237b11d43a458ea7fc5e92f01387bc337f9523
Arthur Alekseev <57834711+arthur100500@users.noreply.github.com>
2022-12-26 02:54:36 +0300

    Variant can now be chosen

--- js/main.js
+++ js/main.js
@@ -1,21 +1,54 @@
 // DOM ELEMENTS
 const canv = document.querySelector("#main");
+const game = document.querySelector("#game");
 const attemptCounter = document.querySelector("#attempts");
+const winMatchedCnt = document.querySelector("#matched-cnt");
+const winTimeCnt = document.querySelector("#time-cnt");
+const winAttemptCnt = document.querySelector("#attempt-cnt");
+const winPercentCnt = document.querySelector("#correct-percent");
+const winDiff = document.querySelector("#diff-cnt");
+const winDiv = document.querySelector("#scores");
+const restartButton = document.querySelector("#restart");
+const variantsDiv = document.querySelector("#variants");
 
 // CONSTANTS
 const delayTime = 1000;
 // should be in cards directory
 // should contain a.mp3 and i.png
-const filenames_mini = ["notification", "skull", "toilet", "xmax"];
-const filenames = ["saul", "amogus", "waltuh", "moyai", "hector", "iphone", "bonk", "glass", "avocado", "natalya", "notification", "skull", "xmax", "pufferfish"];
+const all_filenames = ["saul", "amogus", "waltuh", "moyai", "hector", "iphone", "bonk", "glass", "avocado", "natalya", "notification", "skull", "xmax", "pufferfish", "toilet"];
 const attemptText = "Wrongs: ";
-const frcount = 7;
+const matchedText = "Cards matched: ";
+const timeText = "Time spent: ";
+const attemptCntText = "Times clicked: ";
+const percentText = "Accuracy: ";
+const beatenText = "You have beaten: ";
 
 // GLOBAL VARS
 var selected = null;
 var blocked = false;
 var allCards = [];
 var attempts = 0;
+var corrects = 0;
+var start = null;
+var frcount = 2;
+var diffChosen = null;
+var filenames = [];
+
+// SHUFFLE FILES
+all_filenames_shuffled = all_filenames
+		.map(value => ({ value, sort: Math.random() }))
+		.sort((a, b) => a.sort - b.sort)
+		.map(({ value }) => value);
+
+// VARIANTS
+const variants = {
+	"2 pairs (⭐)" : {width: 2, cards: all_filenames_shuffled.slice(0, 2)}, 
+	"4 pairs (⭐⭐)" : {width: 4, cards: all_filenames_shuffled.slice(0, 4)}, 
+	"6 pairs (⭐⭐)" : {width: 4, cards: all_filenames_shuffled.slice(0, 6)}, 
+	"8 pairs (⭐⭐⭐)" : {width: 4, cards: all_filenames_shuffled.slice(0, 8)},
+	"12 pairs (⭐⭐⭐⭐)" : {width: 6, cards: all_filenames_shuffled.slice(0, 12)},
+}
+
 
 function genCards(){
 	for (const element of filenames) {
@@ -50,6 +83,9 @@ function selectCard(card){
 	if (card.isSelected)
 		return;
 	
+	if (start == null)
+		start = Date.now();
+	
 	card.selectItem();
 	
 	if ((selected == null)){
@@ -59,6 +95,9 @@ function selectCard(card){
 	
 	if (selected.img == card.img){
 		setTimeout(() => {blocked = false;});
+		corrects += 2;
+		if (corrects === allCards.length)
+			showWinScreen();
 	}
 	else {
 		let selectedcp = selected;
@@ -122,8 +161,54 @@ function setGrid(){
 	}
 }
 
-genCards();
-updateAttempts();
+function prettifyTime(ms){
+	let s = Math.floor(ms / 1000) % 60;
+	let m = Math.floor(ms / 1000 / 60) % 60;
+	let h = Math.floor(ms / 1000 / 60 / 60);
+	
+	return h + ":" + m + ":" + s;
+}
 
-setGrid();
+function showWinScreen(){
+	let elapsed = Date.now() - start;
+	function showScore(){
+		game.style.display = 'none';
+		
+		winMatchedCnt.innerHTML = matchedText + "<b>" + filenames.length + "</b>";
+		winTimeCnt.innerHTML = timeText + "<b>" + prettifyTime(elapsed) + "</b>";
+		winAttemptCnt.innerHTML = attemptCntText + "<b>" + (attempts + filenames.length) + "</b>";
+		winPercentCnt.innerHTML = percentText + "<b>" + Math.floor(filenames.length * 2 / (attempts + filenames.length * 2) * 100) + "%" + "</b>";
+		winDiff.innerHTML = beatenText + "<b>" + diffChosen + "</b>";
+		winDiv.className = "scores-show";
+	}
+	game.className = "null-opacity";
+	setTimeout(showScore, 1000);
+}
+
+function startGame(){
+	variantsDiv.style.display = "none";
+	genCards();
+	updateAttempts();
+	setGrid();
+}
+
+function selectFilenames(variant){
+	for (let variant in variants){
+		let elem = document.createElement("div");
+		elem.innerHTML = variant;
+		elem.className = "var-option";
+		elem.onclick = (() => {
+			frcount = variants[variant].width;
+			filenames = variants[variant].cards;
+			setTimeout(startGame, 1000);
+			variantsDiv.className += " null-opacity";
+			diffChosen = variant;
+		});
+		variantsDiv.appendChild(elem);
+	}
+}
+
+selectFilenames("4 pairs");
+
 addEventListener("resize", (event) => {setGrid();});
+restartButton.onclick = (() => {location.reload();});
